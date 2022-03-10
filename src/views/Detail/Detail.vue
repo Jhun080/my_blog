7de1dd42a46aaf8bba7a8200feb7f80a88fb52d0
Jhun <9805325+jhun080@user.noreply.gitee.com>
2022-03-10 15:05:27 +0800

    2022-03-10 15:05

---
+++ src/views/Detail/Detail.vue
@@ -0,0 +1,21 @@
+<!-- 展示某个文章的详细内容 -->
+<template>
+  <div>
+    详细
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Detail',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
