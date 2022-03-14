ac1828c22cc522ec1cf6a60d943a5a0cd637ffc0
Jhun <9805325+jhun080@user.noreply.gitee.com>
2022-03-14 19:49:08 +0800

    添加的文章分页查询功能，以及首页文章列表底部添加了分页栏

--- src/api/index.js
+++ src/api/index.js
@@ -8,9 +8,25 @@ import requests from './ajax'
   无参数
 */
 export const reqGetAllArticle = () => {
-  // 发请求:axios返回的结果是promise对象
   return requests({
     url: '/article/findAllArticle',
     method: 'get'
+  })
+}
+
+/*
+  分页查询文章
+  /api/article/findArticlePage
+  get
+  参数：
+  currentPage：当前页码
+  pageSize：每页显示数据条数
+  query：查询条件
+*/
+export const reqGetArticlePage = (params) => {
+  return requests({
+    url: '/article/findArticlePage',
+    method: 'post',
+    data: params
   })
 }
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -8,6 +8,17 @@
         :article="article"
       >
       </List>
+      <!-- 分页栏 -->
+      <!--  -->
+      <el-pagination
+        :current-page="currentPage"
+        :page-sizes="[2, 3, 4]"
+        :page-size="pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange">
+    </el-pagination>
     </div>
 </template>
 
@@ -20,26 +31,55 @@ export default {
     return {
       // 文章列表数据
       articleList: [],
-      fullscreenLoading: false
+      fullscreenLoading: false,
+      currentPage: 1, // 当前页码
+      pageSize: 2, // 每页显示数据条数
+      total: 0, // 数据总条数
+      query: ''// 查询条件
     }
   },
   components: {
     List
   },
   mounted () {
-    this.getAllArticle()
+    // 分页查询文章数据
+    this.getArticleData()
   },
   methods: {
     // 获取所有文章
-    async getAllArticle () {
-      // 开启页面加载
-      this.fullscreenLoading = true
-      const result = await this.$API.reqGetAllArticle()
+    // async getAllArticle () {
+    //   // 开启页面加载
+    //   // this.fullscreenLoading = true
+    //   const result = await this.$API.reqGetAllArticle()
+    //   if (result.code === 200) {
+    //     this.articleList = result.data
+    //   }
+    //   // 关闭页面加载
+    //   // this.fullscreenLoading = false
+    // },
+    // 分页查询文章数据
+    async getArticleData () {
+      const result = await this.$API.reqGetArticlePage({
+        currentPage: this.currentPage,
+        pageSize: this.pageSize,
+        query: this.query
+      })
       if (result.code === 200) {
-        this.articleList = result.data
+        this.articleList = result.data.rows
+        this.total = result.data.total
       }
-      // 关闭页面加载
-      this.fullscreenLoading = false
+    },
+    // 分页栏页码改变事件
+    handleCurrentChange (val) {
+      this.currentPage = val
+      // 分页查询
+      this.getArticleData()
+    },
+    // 分页栏每页显示条数修改事件
+    handleSizeChange (val) {
+      this.pageSize = val
+      // 分页查询
+      this.getArticleData()
     }
   }
 }
