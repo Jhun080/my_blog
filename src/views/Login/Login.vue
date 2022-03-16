f1338f09fc30c66ef60ece280e4aaa628217d84e
Jhun <9805325+jhun080@user.noreply.gitee.com>
2022-03-16 23:07:32 +0800

    新增登录组件文件

---
+++ src/views/Login/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Login页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
