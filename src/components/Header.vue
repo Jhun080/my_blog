4e1ba6f030ced3f40f4b2f140c5211c02d332b18
Jhun <9805325+jhun080@user.noreply.gitee.com>
2022-03-16 23:03:10 +0800

    添加了登录、后台管理按钮

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -26,7 +26,10 @@
               <i class="el-icon-search" ref="searchIcon"></i>
             </template>
           </el-input>
-          <!-- <div>登录</div> -->
+          <!-- 登录按钮 -->
+          <div class="login" @click="toLogin">登录</div>
+          <!-- 后台管理按钮-->
+          <div class="toManage" @click="toManage">后台管理</div>
         </div>
       </div>
     </header>
@@ -49,6 +52,14 @@ export default {
         this.$router.push({ path: '/list', query: { search: this.search } })
         this.search = ''
       }
+    },
+    // 跳转至登录界面
+    toLogin () {
+      this.$router.push('/login')
+    },
+    // 跳转至后台管理
+    toManage () {
+      this.$router.push('/manage')
     }
   }
 }
@@ -60,10 +71,9 @@ header {
   position: fixed;
   z-index: 1;
   top: 0px;
-
   display: flex;
   justify-content: space-around;
-  background-color: rgba(40,42,44,.6);
+  background-color: rgba(40, 42, 44, 0.6);
   color: white;
   width: 100vw;
   height: 40px;
@@ -87,9 +97,40 @@ header {
   }
   .header-right {
     line-height: 40px;
-    /deep/ .el-input__inner {
-      border: 0;
-      height: 30px;
+    display: flex;
+    justify-content: center;
+
+    .search {
+      line-height: 40px;
+      /deep/ .el-input__inner {
+        line-height: 40px;
+        border: 0;
+        height: 25px;
+      }
+    }
+
+    .login {
+      margin-left: 10px;
+      width: 50px;
+      text-align: center;
+    }
+
+    .login:hover{
+      color:rgb(144, 147, 148);
+      cursor: pointer;
+      transition: all .5s;
+    }
+
+    .toManage {
+      margin-left: 10px;
+      width: 100px;
+      text-align: center;
+    }
+
+    .toManage:hover{
+      color:rgb(144, 147, 148);
+      cursor: pointer;
+      transition: all .5s;
     }
   }
 }
