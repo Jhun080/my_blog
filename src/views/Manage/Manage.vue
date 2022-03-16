d3ca43ced86497849f17e71e3e04918a4ac65438
Jhun <9805325+jhun080@user.noreply.gitee.com>
2022-03-16 23:07:45 +0800

    新增后台管理组件文件

---
+++ src/views/Manage/Manage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    后台管理界面
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Manage',
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+    // 隐藏头部菜单(header)
+    this.hideHeader()
+  },
+  methods: {
+    // 隐藏头部菜单(header)
+    hideHeader () {
+      console.log(this.$parent.$refs.header.style.display = 'none')
+    }
+
+  }
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
