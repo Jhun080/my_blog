0d6c40eef57d574f6e3647f0810a522b1176a830
Jhun <9805325+jhun080@user.noreply.gitee.com>
2022-03-16 23:07:01 +0800

    将首页组件迁移至此文件

---
+++ src/views/Main/Main.vue
@@ -0,0 +1,60 @@
+<template>
+  <!-- 内容部分 -->
+    <div class="main-content">
+      <el-row :gutter="1">
+        <!-- 根据路由显示的内容 -->
+        <el-col class="col-left">
+          <router-view></router-view>
+        </el-col>
+        <!-- 固定部分 标签分类、专栏…………-->
+        <el-col class="col-right">
+          <Sidebar></Sidebar>
+        </el-col>
+      </el-row>
+    </div>
+</template>
+
+<script>
+import Sidebar from '@/views/Sidebar/Sidebar.vue'
+
+export default {
+  name: 'Main',
+  components: {
+    Sidebar
+  }
+}
+</script>
+
+<style lang='less' scoped>
+.main-content {
+  //margin-top: 80px;
+
+  .el-row {
+    margin: 0px !important;
+  }
+
+  .col-left {
+    display: flex;
+    justify-content: center;
+    width: 63vw;
+  }
+
+  .col-right {
+    width: 33vw;
+  }
+}
+
+@media only screen and (max-width: 1000px) {
+  .main-content {
+    display: flex;
+    justify-content: center;
+    .col-left {
+      width: 100vw;
+    }
+
+    .col-right {
+      display: none;
+    }
+  }
+}
+</style>
