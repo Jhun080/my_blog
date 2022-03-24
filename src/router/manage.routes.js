a73637830eedb2307c1d8ca6ea4a537075e798d3
Jhun <9805325+jhun080@user.noreply.gitee.com>
2022-03-24 23:02:26 +0800

    新增后台管理系统路由

--- src/router/manage.routes.js
+++ src/router/manage.routes.js
@@ -1,9 +1,17 @@
 // 后台管理路由
 export default {
   path: '/manage',
-  name: 'Manage',
+  // name: 'Manage',
   component: () => import('@/views/Manage/Manage.vue'),
   children: [
-
+    {
+      path: '/',
+      redirect: '/manage/datamanage'
+    },
+    {
+      path: 'datamanage',
+      name: 'DataManage',
+      component: () => import('@/views/Manage/DataManage/DataManage.vue')
+    }
   ]
 }
