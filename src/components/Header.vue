282c5b6df618ce305acf76abb556e08c1ab38970
Jhun <9805325+jhun080@user.noreply.gitee.com>
2022-03-24 23:01:00 +0800

    对导航栏样式进行改动

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,30 +6,26 @@
         <!-- 导航栏 -->
         <div class="header-left">
           <ul>
-            <li>
-              <router-link to="/">
-                <i class="el-icon-s-shop"></i>
-                首页
-              </router-link>
-            </li>
-            <li class="active"><i class="el-icon-s-order"></i>归档</li>
+            <li :class="{active:tab=='Home'}" @click="toHome"><i class="el-icon-s-shop"></i>首页</li>
+            <li :class="{active:tab=='Archive'}" @click="toArchive"><i class="el-icon-s-order"></i>归档</li>
             <li><i class="el-icon-s-comment"></i>留言</li>
             <li><i class="el-icon-user-solid"></i>关于</li>
             <li><i class="el-icon-s-help"></i>占位</li>
             <li><i class="el-icon-s-help"></i>占位</li>
           </ul>
         </div>
-        <!-- 搜索框 -->
+
         <div class="header-right">
-          <el-input placeholder="请输入你要搜索的关键词">
+          <!-- 搜索框 -->
+          <!-- <el-input placeholder="请输入你要搜索的关键词" v-model="query" @keyup.enter.native="findData">
             <template slot="suffix">
               <i class="el-icon-search" ref="searchIcon"></i>
             </template>
-          </el-input>
+          </el-input> -->
           <!-- 登录按钮 -->
-          <div class="login" @click="toLogin">登录</div>
+          <!-- <div class="login" @click="toLogin">登录</div> -->
           <!-- 后台管理按钮-->
-          <div class="toManage" @click="toManage">后台管理</div>
+          <!-- <div class="toManage" @click="toManage">后台管理</div> -->
         </div>
       </div>
     </header>
@@ -40,10 +36,20 @@ export default {
   name: 'Header',
   data () {
     return {
-      search: ''
+      search: '',
+      query: ''
+    }
+  },
+  computed: {
+    tab () {
+      return this.$route.name
     }
   },
   methods: {
+    // 查询文章数据
+    findData () {
+      this.$store.state.query = this.query
+    },
     onSubmit () {
       return false
     },
@@ -60,6 +66,14 @@ export default {
     // 跳转至后台管理
     toManage () {
       this.$router.push('/manage')
+    },
+    // 跳转至首页
+    toHome () {
+      this.$router.push('/')
+    },
+    // 跳转至归档
+    toArchive () {
+      this.$router.push('/main/archive')
     }
   }
 }
@@ -87,9 +101,11 @@ header {
       padding: 10px 20px;
       animation-name: example;
       animation-duration: 4s;
+      transition: all .5s;
     }
     li:hover {
       background-color: rgb(88, 85, 113);
+      transition: all .5s;
     }
     li.active {
       background-color: rgb(88, 85, 113);
