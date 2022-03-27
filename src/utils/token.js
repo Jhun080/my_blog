3bc8fbb36b8dd4431422d24d7d470997d13780eb
Jhun <9805325+jhun080@user.noreply.gitee.com>
2022-03-27 13:26:40 +0800

    将token存储方式由sessionStorage修改为localStorage

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,14 +1,14 @@
 // 保存用户token
 export const setToken = (token) => {
-  sessionStorage.setItem('user_token', token)
+  localStorage.setItem('user_token', token)
 }
 
 // 获取用户token
 export const getToken = () => {
-  return sessionStorage.getItem('user_token')
+  return localStorage.getItem('user_token')
 }
 
 // 删除用户token
 export const removeToken = () => {
-  sessionStorage.removeItem('user_token')
+  localStorage.removeItem('user_token')
 }
