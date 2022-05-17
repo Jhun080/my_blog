e9469c1ec22ad7406cfc9fd9bf2a24e3e7c402d0
Jhun <9805325+jhun080@user.noreply.gitee.com>
2022-05-17 22:58:43 +0800

    修复了网站已运行时间处理的bug

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,20 +1,14 @@
 <template>
   <div class="footer-nav">
-    <div class="time-nav">
-      网站已运行{{ showTime }} (●'◡'●)ﾉ♥
-    </div>
+    <div class="time-nav">网站已运行{{ showTime }} (●'◡'●)ﾉ♥</div>
     <div class="record-num">
       <a href="https://beian.miit.gov.cn/">闽ICP备2021014840号-1</a>
     </div>
-    <div class="author-info">
-      Made by <a href="https://github.com/Jhun080">Jhun</a>.
-    </div>
+    <div class="author-info">Made by <a href="https://github.com/Jhun080">Jhun</a>.</div>
   </div>
 </template>
 
 <script>
-import dayjs from 'dayjs'
-
 export default {
   name: 'Footer',
   data () {
@@ -27,20 +21,38 @@ export default {
   },
   methods: {
     updateTime () {
-      const originTime = 1646409600000 // 2022-03-05
+      const originTime = 1646408600000 // 2022-03-05
       // 持续时间
       setInterval(() => {
+        // 获取运行时间
         const continueTime = new Date().getTime() - originTime
-        this.showTime = dayjs(continueTime).format(' DD 天 HH 小时 mm 分 ss 秒')
-        // console.log(this.showTime)
+        // 转换时间格式并赋值
+        this.showTime = this.changeTime(continueTime)
       }, 1000)
+    },
+    // 转换时间格式
+    changeTime (continueTime) {
+      continueTime /= 1000
+      // 转换运行时间格式
+      let day = parseInt(continueTime / 60 / 60 / 24)
+      day = day < 10 ? '0' + day : day
+      // 得到小时 格式化成前缀加零的样式
+      let hour = parseInt((continueTime / 60 / 60) % 24)
+      hour = hour < 10 ? '0' + hour : hour
+      // 得到分钟 格式化成前缀加零的样式
+      let minute = parseInt((continueTime / 60) % 60)
+      minute = minute < 10 ? '0' + minute : minute
+      // 得到秒 格式化成前缀加零的样式
+      let sec = parseInt(continueTime % 60)
+      sec = sec < 10 ? '0' + sec : sec
+      return ` ${day} 天 ${hour} 小时 ${minute} 分 ${sec} 秒 `
     }
   }
 }
 </script>
 
-<style lang='less' scoped>
-.footer-nav{
+<style lang="less" scoped>
+.footer-nav {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
@@ -51,29 +63,28 @@ export default {
   font-size: 14px;
   text-align: center;
   background-color: rgb(31, 30, 30);
-  transition: all .5s;
+  transition: all 0.5s;
 
-  .time-nav{
-
+  .time-nav {
   }
 
-  .record-num{
+  .record-num {
     width: 100%;
     margin: 5px 0px;
   }
 
-  .author-info{
+  .author-info {
     width: 100%;
   }
 }
 
-a{
+a {
   color: white;
   text-decoration: none;
-  transition: all .5s;
+  transition: all 0.5s;
 }
-a:hover{
+a:hover {
   color: rgb(202, 201, 201);
-  transition: all .5s;
+  transition: all 0.5s;
 }
 </style>
